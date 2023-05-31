682a5704eaae7c6620e290280b9cedfe5836951a
Kunal <dhongadikunal@gmail.com>
2023-05-31 13:55:37 +0530

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['render-tutorial.onrender.com']
+ALLOWED_HOSTS = ['polaroid-il06.onrender.com']
 
 
 # Application definition
